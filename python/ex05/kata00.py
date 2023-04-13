db9807ce6d60fe26844f2fc3887092d6a8be0abb
Pedro Reina Ganan <preina-g@c2r5s2.42malaga.com>
2023-04-13 13:30:07 +0200

    python module 00 terminado

--- python/ex05/kata00.py
+++ python/ex05/kata00.py
@@ -6,12 +6,12 @@
 #    By: preina-g <preina-g@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/11 18:23:27 by preina-g          #+#    #+#              #
-#    Updated: 2023/04/13 13:05:07 by preina-g         ###   ########.fr        #
+#    Updated: 2023/04/13 13:29:51 by preina-g         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # Put this at the top of your kata00.py file
-kata = (19,)
+kata = (19,42,21)
 
 s = ', '.join(map(str, kata))
 out = "The {} numbers are: {}"
